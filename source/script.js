6b42e3936fa0de123b911e5b106243b694d3cafc
Logout-01 <apichream@gmail.com>
2019-03-19 18:48:40 +0700

    Raindow circle

--- source/script.js
+++ source/script.js
@@ -52,41 +52,60 @@ window.onload = function () {
             this.show = function(){
                 phys.noStroke();
                 phys.ellipse(this.x,this.y,25);
-            }
+            };
             this.moveX = function(){
                 if(this.x > phys.width - 13 || this.x < 13){
                     this.velocity = -(this.velocity);
                     phys.fill(phys.random(255), phys.random(255), phys.random(255));
                 }
                 this.x += this.velocity;
-            }
+            };
+            this.moveY = function () {
+                if (this.y > phys.height - 13 || this.y < 13) {
+                    this.velocity = -(this.velocity);
+                    phys.fill(phys.random(255), phys.random(255), phys.random(255));
+                }
+                this.y += this.velocity;
+            };
         }
 
         phys.setup = function(){
             phys.createCanvas(400, 400);
-            first_circle = new Circle(2, 40, 170);
-            second_circle = new Circle(-2, 360,170);
+            first_circle = new Circle(2, 40, 200);
+            second_circle = new Circle(-2, 360,200);
+            third_circle = new Circle(2, 200, 40);
+            forth_circle = new Circle(-2, 200, 360);
         };
         
         phys.draw = function(){
             var colorR = phys.random(0,255);
             var colorG = phys.random(0,255);
             var colorB = phys.random(0,255);
+            phys.fill(phys.random(255), phys.random(255), phys.random(255));
             phys.background(0);
             phys.line(first_circle.x, first_circle.y, second_circle.x, second_circle.y);
+            phys.line(third_circle.x, third_circle.y, forth_circle.x, forth_circle.y);
             first_circle.show();
             first_circle.moveX();
             second_circle.show();
             second_circle.moveX();
-            distance = Math.sqrt((first_circle.x - second_circle.x)*(first_circle.x - second_circle.x)+(first_circle.y - second_circle.y)*(first_circle.y - second_circle.y));
-            let string = phys.map(distance, 25, 350, 10, 2);
-            let stringColor = phys.map(distance, 25, 350, 150,255);
+            third_circle.show();
+            third_circle.moveY();
+            forth_circle.show();
+            forth_circle.moveY();
+            distanceAB = Math.sqrt((first_circle.x - second_circle.x)*(first_circle.x - second_circle.x)+(first_circle.y - second_circle.y)*(first_circle.y - second_circle.y));
+            distanceCD = Math.sqrt((third_circle.x - forth_circle.x) * (third_circle.x - forth_circle.x) + (third_circle.y - forth_circle.y) * (third_circle.y - forth_circle.y));
+            let string = phys.map(distanceAB, 25, 350, 10, 2);
+            let stringColor = phys.map(distanceAB, 25, 350, 150,255);
             phys.stroke(stringColor);
             phys.strokeWeight(string);
-            if(distance < 25){
+            if(distanceAB < 25){
                 first_circle.velocity = -(first_circle.velocity);
                 second_circle.velocity = -(second_circle.velocity);
-                phys.fill(phys.random(255), phys.random(255), phys.random(255));
+            }
+            if (distanceCD < 25) {
+                third_circle.velocity = -(third_circle.velocity);
+                forth_circle.velocity = -(forth_circle.velocity);
             }
         };
             
