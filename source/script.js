595beedf0cce91bf791d7f54533361107c9ad129
Logout-01 <apichream@gmail.com>
2019-03-19 11:45:43 +0700

    Added another canvas, Added Random movement circle

--- source/script.js
+++ source/script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     var simpleSketch = function (sketch) {
-        
+
         sketch.setup = function () {
             sketch.createCanvas(400, 400);
             sketch.background(0);
@@ -27,16 +27,41 @@ window.onload = function () {
         };
         
         $.draw = function () {
+            $.noStroke();
+            $.ellipse($.mouseX, $.mouseY, 25);
+        };
+
+        $.mousePressed = function(){
             var colorR = $.random(0,255);
             var colorG = $.random(0,255);
             var colorB = $.random(0,255);
-            $.noStroke();
-            $.fill(colorR, colorG, colorB, 50);
-            $.ellipse($.mouseX, $.mouseY, 25,25);
-
+            $.fill(colorR, colorG, colorB,150);
         };
     };
 
+    var physicSketch = function (phys) {
+        
+        var x = phys.mouseX, y = phys.mouseY;
+        phys.setup = function(){
+            phys.createCanvas(400, 400);
+        }
+        phys.draw = function(){
+            var colorR = phys.random(0, 255);
+            var colorG = phys.random(0, 255);
+            var colorB = phys.random(0, 255);
+            phys.background(0);
+            phys.noStroke();
+            x = x + phys.random(-3,3);
+            y = y + phys.random(-3,3);
+            phys.fill(colorR, colorG, colorB);
+            // phys.mousePressed = function(){
+                phys.ellipse(x,y,25);
+            // }
+        };
+            
+        }
+
     var first_sketch = new p5(simpleSketch, 'first-sketch-container');
     var second_sketch = new p5(touchSketch, 'second-sketch-container');
+    var third_sketch = new p5(physicSketch, 'third-sketch-container');
 }
